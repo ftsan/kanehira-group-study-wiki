e020fdc567e87867a99e21983318aeacc09c95f8
ftsan <ftsan@gmail.com>
2014-05-24 13:33:08 +0900

    create Wiki

---
+++ src/main/java/co/jp/rakus/entity/Wiki.java
@@ -0,0 +1,11 @@
+package co.jp.rakus.entity;
+
+import java.util.Date;
+
+public class Wiki {
+	public int id;
+	public String title;
+	public String archive;
+	public Date createDate;
+	public Date updateDate;
+}
